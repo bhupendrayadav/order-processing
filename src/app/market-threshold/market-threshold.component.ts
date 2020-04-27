1a6594364a81e2b8bce94aa2ab43f1f72e4c2e15
krunalujwal <krunalujwal@gmail.com>
2020-04-27 20:59:55 +0530

    Integrated Code

--- src/app/market-threshold/market-threshold.component.ts
+++ src/app/market-threshold/market-threshold.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { MarketThresholdService } from '../service/market-threshold.service';
+import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface ThresholdValue {
+  MKT_VAL_FROM: number;
+  MKT_VAL_TO: number;
+  VAR_THRESHOLD: number;
+  REPDEL_INCL: boolean;
+  Client: string;
+  CREATED_ON?: any;
+  CREATED_BY?: any;
+  UPDATED_BY?: any;
+  UPDATED_ON?: any;
+}
 
 @Component({
   selector: 'app-market-threshold',
@@ -7,16 +21,88 @@ import { MarketThresholdService } from '../service/market-threshold.service';
   styleUrls: ['./market-threshold.component.css']
 })
 export class MarketThresholdComponent implements OnInit {
-  variances: any = [];
-  constructor(private marketThresholdService: MarketThresholdService) { }
+  page = 1;
+  pageSize = 3;
+  mvtData: Array<ThresholdValue> = [];
+  modalReference: NgbModalRef;
+
+  isFormSubmitted = false;
+  mvtForm: FormGroup;
+  selectedRecord: ThresholdValue;
+  selectedRecordIndex: number;
+
+  constructor(private marketThresholdService: MarketThresholdService,
+    private modalService: NgbModal,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
     this.getMarketThresholds();
   }
 
+  get marketValueThreshold(): ThresholdValue[] {
+    if (this.mvtData && this.mvtData.length) {
+      return this.mvtData
+        .map((country, i) => ({ id: i + 1, ...country }))
+        .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+    }
+  }
+
   getMarketThresholds() {
     this.marketThresholdService.getMarketThreshold().subscribe(res => {
-      this.variances = res
+      this.mvtData = res;
     });
   }
+
+  onRefresh() {
+    this.page = 1;
+    this.pageSize = 3;
+    this.getMarketThresholds();
+  }
+
+  addNewRecord(content, data?: ThresholdValue) {
+    this.mvtForm = this.fb.group({
+      MKT_VAL_FROM: ['', [Validators.required, Validators.pattern(/^[0-9]*$/)]],
+      MKT_VAL_TO: ['', [Validators.required, Validators.pattern(/^[0-9]*$/)]],
+      VAR_THRESHOLD: ['', [Validators.required, Validators.pattern(/^[0-9]*$/)]],
+    });
+    if (data) {
+      this.mvtForm.patchValue(data);
+    } else {
+      this.selectedRecordIndex = null;
+      this.selectedRecord = null;
+    }
+    this.modalReference = this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
+  }
+
+  closeModal() {
+    this.modalReference.close();
+  }
+
+  onSubmit() {
+    this.isFormSubmitted = true;
+    if (this.mvtForm.valid) {
+      if (this.selectedRecordIndex !== null) {
+        this.mvtData[this.selectedRecordIndex] = this.mvtForm.value;
+        this.marketValueThreshold[this.selectedRecordIndex] = this.mvtForm.value;
+        this.selectedRecordIndex = null;
+        this.selectedRecord = null;
+      } else {
+        this.mvtData.push(this.mvtForm.value);
+        this.marketValueThreshold.push(this.mvtForm.value);
+      }
+      this.closeModal();
+    }
+  }
+
+  onDelete(index) {
+    this.mvtData.splice(index, 1);
+    this.marketValueThreshold.splice(index, 1);
+  }
+
+  onEdit(content, index) {
+    this.selectedRecordIndex = index;
+    this.selectedRecord = this.mvtData[index];
+    this.addNewRecord(content, this.selectedRecord);
+  }
+
 }
--- src/app/market-threshold/market-threshold.module.ts
+++ src/app/market-threshold/market-threshold.module.ts
@@ -1,5 +1,8 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { MarketThresholdRoutingModule } from './market-threshold-routing.module';
 import { MarketThresholdComponent } from './market-threshold.component';
@@ -9,7 +12,10 @@ import { MarketThresholdComponent } from './market-threshold.component';
   declarations: [MarketThresholdComponent],
   imports: [
     CommonModule,
-    MarketThresholdRoutingModule
+    MarketThresholdRoutingModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class MarketThresholdModule { }
