28e0240c82587d56af4968b2839b68e3b4d4d14e
krshmanish <krshmanish@gmail.com>
2020-05-06 16:34:35 +0530

    Created UI based on design provied.

--- src/app/dashboard/manager/manager.component.ts
+++ src/app/dashboard/manager/manager.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { UsersService } from 'src/app/service/users.service';
+import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 interface users {
   userId: number;
@@ -13,20 +14,212 @@ interface users {
   styleUrls: ['./manager.component.css']
 })
 export class ManagerComponent implements OnInit {
+  // For user search
   search: FormControl = new FormControl();
   usersList: users[] = [];
 
-  constructor(private dashboardService: UsersService) { }
+  // For Assign modal
+  assignModalReference: NgbModalRef;
+  searchUser: FormControl = new FormControl();
+
+  // For table
+  filteredTaskDetails: any[];
+  taskDetails: any[] = [
+    {
+      taskName: "Desktop Fulfillment",
+      orderNumber: "98000296",
+      loanNumber: "338345254",
+      transactionType: "Refiance",
+      lenderName: "USAA BULK-PARENT",
+      submitted: "",
+      age: "25",
+      clientduedate: "02/15/2019",
+      borrower: "",
+      address: "",
+      country: "",
+      state: "",
+      loanPurpose: "",
+      status: "",
+      dueDate: "",
+    },
+    {
+      taskName: "Desktop Fulfillment",
+      orderNumber: "98000297",
+      loanNumber: "438345254",
+      transactionType: "Refiance",
+      lenderName: "USAA BULK-PARENT",
+      submitted: "",
+      age: "30",
+      clientduedate: "02/15/2019",
+      borrower: "",
+      address: "",
+      country: "",
+      state: "",
+      loanPurpose: "",
+      status: "",
+      dueDate: "",
+    },
+    {
+      taskName: "Fulfillment",
+      orderNumber: "98000298",
+      loanNumber: "238345254",
+      transactionType: "Refiance",
+      lenderName: "USAA BULK-PARENT",
+      submitted: "",
+      age: "20",
+      clientduedate: "02/15/2019",
+      borrower: "",
+      address: "",
+      country: "",
+      state: "",
+      loanPurpose: "",
+      status: "",
+      dueDate: "",
+    },
+    {
+      taskName: "Desktop Fulfillment",
+      orderNumber: "98000296",
+      loanNumber: "338345254",
+      transactionType: "Refiance",
+      lenderName: "USAA BULK-PARENT",
+      submitted: "",
+      age: "25",
+      clientduedate: "02/15/2019",
+      borrower: "",
+      address: "",
+      country: "",
+      state: "",
+      loanPurpose: "",
+      status: "",
+      dueDate: "",
+    },
+    {
+      taskName: "Desktop Fulfillment",
+      orderNumber: "98000296",
+      loanNumber: "338345254",
+      transactionType: "Refiance",
+      lenderName: "USAA BULK-PARENT",
+      submitted: "",
+      age: "25",
+      clientduedate: "02/15/2019",
+      borrower: "",
+      address: "",
+      country: "",
+      state: "",
+      loanPurpose: "",
+      status: "",
+      dueDate: "",
+    },
+    {
+      taskName: "Desktop Fulfillment",
+      orderNumber: "98000296",
+      loanNumber: "338345254",
+      transactionType: "Refiance",
+      lenderName: "USAA BULK-PARENT",
+      submitted: "",
+      age: "25",
+      clientduedate: "02/15/2019",
+      borrower: "",
+      address: "",
+      country: "",
+      state: "",
+      loanPurpose: "",
+      status: "",
+      dueDate: "",
+    },
+    {
+      taskName: "Desktop Fulfillment",
+      orderNumber: "98000296",
+      loanNumber: "338345254",
+      transactionType: "Refiance",
+      lenderName: "USAA BULK-PARENT",
+      submitted: "",
+      age: "25",
+      clientduedate: "02/15/2019",
+      borrower: "",
+      address: "",
+      country: "",
+      state: "",
+      loanPurpose: "",
+      status: "",
+      dueDate: "",
+    },
+    {
+      taskName: "Desktop Fulfillment",
+      orderNumber: "98000296",
+      loanNumber: "338345254",
+      transactionType: "Refiance",
+      lenderName: "USAA BULK-PARENT",
+      submitted: "",
+      age: "25",
+      clientduedate: "02/15/2019",
+      borrower: "",
+      address: "",
+      country: "",
+      state: "",
+      loanPurpose: "",
+      status: "",
+      dueDate: "",
+    },
+    {
+      taskName: "Desktop Fulfillment",
+      orderNumber: "98000296",
+      loanNumber: "338345254",
+      transactionType: "Refiance",
+      lenderName: "USAA BULK-PARENT",
+      submitted: "",
+      age: "25",
+      clientduedate: "02/15/2019",
+      borrower: "",
+      address: "",
+      country: "",
+      state: "",
+      loanPurpose: "",
+      status: "",
+      dueDate: "",
+    },
+    {
+      taskName: "Desktop Fulfillment",
+      orderNumber: "98000296",
+      loanNumber: "338345254",
+      transactionType: "Refiance",
+      lenderName: "USAA BULK-PARENT",
+      submitted: "",
+      age: "25",
+      clientduedate: "02/15/2019",
+      borrower: "",
+      address: "",
+      country: "",
+      state: "",
+      loanPurpose: "",
+      status: "",
+      dueDate: "",
+    },
+  ];
+
+  constructor(private dashboardService: UsersService, private modalService: NgbModal) { }
 
   ngOnInit() {
-    this.search.valueChanges.subscribe(
-      term => {
-        if (term.length > 3) {
-          this.dashboardService.getUsersList(term).subscribe(data => {
-            this.usersList = data.items;
-          })
-        }
-      });
+    this.filteredTaskDetails = this.taskDetails;
+
+    this.search.valueChanges.subscribe(term => {
+      if (term.length > 3) {
+        this.dashboardService.getUsersList(term).subscribe(data => {
+          this.usersList = data.items;
+        })
+      }
+    });
   }
 
+  openAssignModel(model) {
+    this.assignModalReference = this.modalService.open(model, { ariaLabelledBy: 'modal-basic-title' });
+  }
+
+  closeAssignModel() {
+    this.assignModalReference.close();
+  }
+
+  onAssign() {
+
+  }
 }
